b20da0c7adde19228ca46c5985f51996f44e01ad
Stian Morsund <stian@stianmorsund.no>
2018-02-12 16:00:37 +0100

    e2e: test for search

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,15 @@ describe('angular-fagkveld App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
+  it('should display welcome message', async() => {
+    await page.navigateTo();
     expect(page.getParagraphText()).toEqual('Andeby');
+  });
+
+  it('should filter results when I search for \"Donald\"', async() => {
+    await page.navigateTo();
+    await page.searchFor('Donald');
+    const numberOfResults = await page.getNumberOfResults();
+    expect(numberOfResults).toBe(1);
   });
 });
