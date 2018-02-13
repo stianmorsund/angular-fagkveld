382c92aace6a66625a9f51cb5e4ae2e7eb520323
Øystein Lygre <oystein.lygre@gmail.com>
2018-02-13 11:34:23 +0100

    fix: cleanup unused imports

--- src/app/citizens/citizen-list/citizen-list.component.ts
+++ src/app/citizens/citizen-list/citizen-list.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 import { CitizensService } from '../citizen-service/citizens.service';
 import { Citizen } from '../citizen.model';
